e0df9c657e42af6e823ae00f0620fa1200a6c75d
daw2-henares22 <91276223+daw2-henares22@users.noreply.github.com>
2022-11-30 06:50:36 -0800

    Add files via upload

---
+++ N5P101comments.php
@@ -0,0 +1,25 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device=width, initial-scale=1.0">
+</head>
+<body>
+    <ol>
+        <li>Tables</li>
+        <li>commit</li>
+        <li>delete</li>
+        <li>add</li>
+        <li>edit</li>
+        <li>ucfirst</li>
+        <li>UPDATE</li>
+        <li>set values to blank</li>
+        <li>type information</li>
+        <li>case</li>
+    </ol>
+    <p>Document: 10. Teacher explanations: 10.</p>
+
+    <p>Own note of the exercise: 10</p>
+
+    <p>Some comands to put in the files, they were not right, if it gets wrong, I prefer that it not be put or wake an example.</p>
+</body>
+</html>
